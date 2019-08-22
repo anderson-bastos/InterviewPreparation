0384e90b769fa27d721abfc43f1127aef599bb7c
Anderson Bastos <anderson.bastos@resourceit.com>
2019-08-22 16:18:43 -0300

    Update preferences

--- src/test/java/com/hackerhank/arrays/LeftRotationTest.java
+++ src/test/java/com/hackerhank/arrays/LeftRotationTest.java
@@ -1,8 +1,6 @@
 package com.hackerhank.arrays;
 
-import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
